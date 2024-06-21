d9c742cf276741c0dc35eb949bc94f222be97228
project774fan <chinari819y2k@gmail.com>
2024-06-22 05:16:50 +0900

    キキボタン

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
+  <Head>
+    {/* <link rel="icon" href={favicon.src} /> */}
+    <title>キキボタン</title>
+    <meta httpEquiv="Content-Language" content="ja" />
+  </Head>;
   return <Component {...pageProps} />;
 }
