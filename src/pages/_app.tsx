9bafbb31c6f32bd9d467be4f3850941abbb79649
project774fan <chinari819y2k@gmail.com>
2024-06-22 14:30:32 +0900

    github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,5 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  <Head>
-    {/* <link rel="icon" href={favicon.src} /> */}
-    <title>キキボタン</title>
-    <meta httpEquiv="Content-Language" content="ja" />
-  </Head>;
   return <Component {...pageProps} />;
 }
